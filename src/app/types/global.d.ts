d62ac34b49e39e462212d626fcee07ffb9c33e34
Segrey Kiselev <kissergey98@mail.ru>
2023-02-09 23:15:30 +0300

    add internationalization i18n

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -10,3 +10,5 @@ declare module "*.svg" {
 }
 
 declare module "*.png"
+
+declare const __IS_DEV__: boolean;
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "app/providers/ThemeProvider";
 import { App } from "app/App";
 
-
 render(
   <BrowserRouter>
     <ThemeProvider>
