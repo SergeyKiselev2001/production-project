338e3e522120c4306dfa20cc7f86dcf7bc6ec5df
SergeyKiselevProduce <kissergey98@mail.ru>
2023-02-10 17:54:53 +0300

    add jest

---
+++ src/shared/lib/helpers/classNames/classNames.test.ts
@@ -0,0 +1,15 @@
+import { classNames } from './classNames'
+
+describe('classNames', () => {
+  test('common case', () => {
+    expect(classNames('class')).toBe('class')
+  })
+
+  test('with additional classes', () => {
+    expect(classNames('class', {}, ['class2', 'class3'])).toBe('class class2 class3')
+  })
+
+  test('with mods', () => {
+    expect(classNames('class', { rule1: true, rule2: false })).toBe('class rule1')
+  })
+})
